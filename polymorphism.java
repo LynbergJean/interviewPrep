a720e228a44719ae207f32af9fae42624d2f07a0
lynbergjean <lynberg281@gmail.com>
2025-12-16 15:25:58 -0500

    poly

---
+++ polymorphism.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+/*
+types of polymorphism
+compile time/static polymorphism
+this can be achieved via method overloading.
+
+
+Dynamic polymorphism/ runtime polymorphism 
+achieved via method overriding
+
+when a child class has the same method has the parent class and we call the child class's method 
+this is called @overrriding. when overriding the parent method.
+
+@override is to check if a method is overriden or not they are used for check purposes.
+
+Parent object = new child;
+the method that will be called will depend of the type of child and this is know as UPCASTING and
+this is also known as Upcasting;
+
+Dynamic method Dispatch: 
+
+Final is used to create constants and can be used to prevent overriding.
+you cannot override a method that is final they provide enhencement. this is called early binding and 
+late binding.
+when you put a class as final you also declare its methods as final also.
+
+a static method is a method that is object independant.
+inside a static method everything inside has to be static as well. 
+no we cannot override a static method. the one in the main class is the one that is being called 
+static methods can be inherited but they cannot be overriden static is object independ hence why
+they cannot be overriden but they can be inherited.
+
+you can inherit but you cannot override
+
+
+
+
+*/
+public class Polymorphism{
+	
+	
