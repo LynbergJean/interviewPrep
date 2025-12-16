f8ec690a4f72fdd3a23e3f7ebbbd2e8575a5aaf9
lynbergjean <lynberg281@gmail.com>
2025-12-16 16:00:38 -0500

    difference between encaps and abstraction

---
+++ encapsulation.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/*
+
+it means wrapping up the implementation of the data members and methods in a class.
+
+can often be confused with abstration:
+which is hiding unecessary details and only showing necessary information
+you have a car and you put in the ignition and start the car you cannot see the process that goes on
+behind the scene into starting the car you cant really see how the fuel injector is working 
+you cannot see the process that goes into the radiator you can only see that you started the car.
+
+you dont need to know all these unecessary details to start the car.
+
+abstraction is solving a design level issue and encapsulation is solving a implementation level issue
+and hides the entire code into a single entity and protects it from the world.
+
+abstratcion is a method of gaining information and encapsulation is a process of containing  the info.
+
+in abstratction we can use the interfaces.
+
+encapsulation is putting data in a bundle and hding it from the world.
+
+*/
